9913c9c57240e64a1bcbd153177dc094cfbc872c
윤석준 <tjrwns8024@naver.com>
2021-03-24 10:48:03 +0900

    [CREATE] project setting

---
+++ src/main/kotlin/com/tjrwns8024/wmms/WmmsApplication.kt
@@ -0,0 +1,11 @@
+package com.tjrwns8024.wmms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WmmsApplication
+
+fun main(args: Array<String>) {
+    runApplication<WmmsApplication>(*args)
+}
---
+++ src/test/kotlin/com/tjrwns8024/wmms/WmmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tjrwns8024.wmms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WmmsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
