5253600db23a2ef431d8cb32a9e06d82544a6c8d
윤석준 <tjrwns8024@naver.com>
2021-03-26 23:56:51 +0900

    [ADD] <service/ RegiserSErviceImpl.kt> add description, registerDate, image

--- src/main/kotlin/com/tjrwns8024/wmms/service/RegisterServiceImpl.kt
+++ src/main/kotlin/com/tjrwns8024/wmms/service/RegisterServiceImpl.kt
@@ -4,14 +4,19 @@ import com.tjrwns8024.wmms.model.entitys.Washer
 import com.tjrwns8024.wmms.model.payload.request.WasherRequest
 import com.tjrwns8024.wmms.repository.RegisterRepository
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class RegisterServiceImpl(private var registerRepository: RegisterRepository) : RegisterService {
     override fun registerWM(washerRequest: WasherRequest) {
+
         registerRepository.save(
                 Washer(
                         name = washerRequest.name,
-                        description = washerRequest.description
+                        description = washerRequest.description,
+                        image = "fsadfas",
+                        register_date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
                 )
         )
     }
