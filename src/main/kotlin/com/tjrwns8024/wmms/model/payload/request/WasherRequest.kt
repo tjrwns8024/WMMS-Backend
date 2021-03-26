237a78e4abea0445c368035ebec349ff791ce98b
윤석준 <tjrwns8024@naver.com>
2021-03-26 23:56:10 +0900

    [ADD] <model/ WasherRequest.kt>

--- src/main/kotlin/com/tjrwns8024/wmms/model/payload/request/WasherRequest.kt
+++ src/main/kotlin/com/tjrwns8024/wmms/model/payload/request/WasherRequest.kt
@@ -1,10 +1,13 @@
 package com.tjrwns8024.wmms.model.payload.request
 
+import org.springframework.web.multipart.MultipartFile
 import javax.validation.constraints.NotEmpty
 
 data class WasherRequest (
     @NotEmpty
     val name: String,
     @NotEmpty
-    val description: String
+    val description: String,
+    @NotEmpty
+    val image: String
 )
