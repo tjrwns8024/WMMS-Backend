acb7a9b8e2927aaaff31e0bd9db6c010dabf1d7a
윤석준 <tjrwns8024@naver.com>
2021-03-29 23:32:24 +0900

    [ADD] 모든 세탁기, 사용가능 세탁기 조회

--- src/main/kotlin/com/tjrwns8024/wmms/repository/WasherRepository.kt
+++ src/main/kotlin/com/tjrwns8024/wmms/repository/WasherRepository.kt
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RegisterRepository : JpaRepository<Washer, Int> {
+interface WasherRepository : JpaRepository<Washer, Int> {
+    fun findAllByStatus(status: Boolean): List<Washer>
+    fun findAllBy(): List<Washer>
 }
