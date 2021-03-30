d194bc3ee2070914761856aac48ba3e8cd52bd32
윤석준 <tjrwns8024@naver.com>
2021-03-30 09:52:37 +0900

    [ADD] InfoController.kt  / 상세정보 api 추가

--- src/main/kotlin/com/tjrwns8024/wmms/controller/InfoController.kt
+++ src/main/kotlin/com/tjrwns8024/wmms/controller/InfoController.kt
@@ -1,6 +1,7 @@
 package com.tjrwns8024.wmms.controller
 
 import com.tjrwns8024.wmms.model.response.WasherInfo
+import com.tjrwns8024.wmms.model.response.WasherListInfo
 import com.tjrwns8024.wmms.service.InfoService
 import org.springframework.web.bind.annotation.*
 
@@ -11,10 +12,14 @@ class InfoController(
 ) {
 
     @GetMapping("/available")
-    fun getAvailableWM(): List<WasherInfo> =
+    fun getAvailableWM(): List<WasherListInfo> =
             infoService.getAvailableWM()
 
     @GetMapping("/all")
-    fun getAllWM(): List<WasherInfo> =
+    fun getAllWM(): List<WasherListInfo> =
             infoService.getAllWM()
+
+    @GetMapping("/detail/{id}")
+    fun getDetailWM(@PathVariable id: Int): WasherInfo =
+            infoService.getDetailWM(id)
 }
