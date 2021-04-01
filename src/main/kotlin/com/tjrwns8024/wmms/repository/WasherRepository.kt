e5e1f33d1a6a6d91f6bbc514a7da5a93f092c27b
윤석준 <tjrwns8024@naver.com>
2021-04-01 11:05:34 +0900

    add findByName

--- src/main/kotlin/com/tjrwns8024/wmms/repository/WasherRepository.kt
+++ src/main/kotlin/com/tjrwns8024/wmms/repository/WasherRepository.kt
@@ -3,9 +3,11 @@ package com.tjrwns8024.wmms.repository
 import com.tjrwns8024.wmms.model.entitys.Washer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface WasherRepository : JpaRepository<Washer, Int> {
     fun findAllByStatus(status: Boolean): List<Washer>
     fun findAllBy(): List<Washer>
+    fun findByName(name: String): Optional<Washer>
 }
