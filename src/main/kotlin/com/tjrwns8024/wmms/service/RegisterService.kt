cf337345ce60e14285e6b4aea5f33006daaa6124
윤석준 <tjrwns8024@naver.com>
2021-04-01 11:02:18 +0900

    DELETE files

--- src/main/kotlin/com/tjrwns8024/wmms/service/RegisterService.kt
+++
@@ -1,7 +0,0 @@
-package com.tjrwns8024.wmms.service
-
-import org.springframework.web.multipart.MultipartFile
-
-interface RegisterService {
-    fun registerWM(name: String, description: String, image: MultipartFile)
-}
