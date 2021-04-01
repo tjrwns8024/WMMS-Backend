64c9621ae2ccaffac9f62644d604d01294e3599b
윤석준 <tjrwns8024@naver.com>
2021-04-01 11:02:46 +0900

    [CREATE] WasherService.kt

---
+++ src/main/kotlin/com/tjrwns8024/wmms/service/WasherService.kt
@@ -0,0 +1,13 @@
+package com.tjrwns8024.wmms.service
+
+import com.tjrwns8024.wmms.model.response.WasherInfo
+import com.tjrwns8024.wmms.model.response.WasherListInfo
+import org.springframework.web.multipart.MultipartFile
+
+interface WasherService {
+    fun registerWM(name: String, description: String, image: MultipartFile): Int
+    fun getAvailableWM(): List<WasherListInfo>
+    fun getAllWM(): List<WasherListInfo>
+    fun getDetailWM(id: Int): WasherInfo
+    fun changeWMStatus(id: Int)
+}
